5059d088a70257eee804d25908dde3f4981a859e
Mathias Salva <45085448+MattSalva@users.noreply.github.com>
2022-08-22 19:18:22 -0300

    First Commit

---
+++ src/main/java/com/portfolio/mathiassalva/MathiassalvaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mathiassalva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathiassalvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathiassalvaApplication.class, args);
+	}
+
+}
