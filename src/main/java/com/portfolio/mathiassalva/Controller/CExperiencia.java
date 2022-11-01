9be9fa5f8cd43911adf69325df8ecddc7fc3d13f
Mathias Salva <mattsalva@outlook.com>
2022-10-31 21:25:13 -0300

    arreglando detalles de back

--- src/main/java/com/portfolio/mathiassalva/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/mathiassalva/Controller/CExperiencia.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("experience")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend-mathiassalva.web.app")
 public class CExperiencia {
     @Autowired
     Sxp sxp;
